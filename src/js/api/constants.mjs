f6efda8f0a3c444f784fb3ca56b7b423ee73c46f
Sirvau <sirols50984@stud.noroff.no>
2024-05-02 09:55:41 +0200

    Api auth constants

---
+++ src/js/api/constants.mjs
@@ -0,0 +1,4 @@
+export const API_BASE = 'https://v2.api.noroff.dev/';
+export const API_REGISTER = '/auth/register';
+export const API_LOGIN = '/auth/login';
+export const API_KEY = '/auth/create-api-key';
