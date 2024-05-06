03fd1727aee7188453f8e575e9240ad8e2a388e3
Sirvau <sirols50984@stud.noroff.no>
2024-05-06 16:32:21 +0200

    Created get listing-function

--- src/js/api/listings/getListings.mjs
+++ src/js/api/listings/getListings.mjs
@@ -1,5 +1,6 @@
-import { API_BASE, API_LISTINGS } from '../constants.mjs';
+import { API_BASE, API_LISTINGS, API_SINGLE_LISTING } from '../constants.mjs';
 
+// Get All Listings
 export async function getListings() {
   try {
     const response = await fetch(API_BASE + API_LISTINGS, {
@@ -12,9 +13,31 @@ export async function getListings() {
     if (response.ok) {
       const listingData = await response.json();
       console.log(listingData);
-      return listingData;
+      return await listingData;
     }
   } catch {
-    throw new Error('response.statusText');
+    throw new Error(response.statusText);
+  }
+}
+
+//Get Single Listing By ID
+export async function getSingleListing(id) {
+  try {
+    const response = await fetch(API_BASE + API_SINGLE_LISTING, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const singleListingData = await response.json();
+      console.log(singleListingData);
+      return await singleListingData;
+    }
+  } catch {
+    if (!id) {
+      throw new Error('Get requires a postID');
+    }
   }
 }
