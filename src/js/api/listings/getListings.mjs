3808eb8509dbb22e628fd93527ebf9fd6cca070c
Sirvau <sirols50984@stud.noroff.no>
2024-05-11 11:45:36 +0200

    Added seller param and active flag

--- src/js/api/listings/getListings.mjs
+++ src/js/api/listings/getListings.mjs
@@ -1,14 +1,22 @@
-import { API_BASE, API_LISTINGS } from '../constants.mjs';
+import {
+  API_ACTIVE,
+  API_BASE,
+  API_LISTINGS,
+  API_SELLER,
+} from '../constants.mjs';
 
 // Get All Listings
 export async function getListings() {
   try {
-    const response = await fetch(API_BASE + API_LISTINGS, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await fetch(
+      `${API_BASE}${API_LISTINGS}?${API_SELLER}&${API_ACTIVE}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    });
+    );
 
     if (response.ok) {
       const listingData = await response.json();
